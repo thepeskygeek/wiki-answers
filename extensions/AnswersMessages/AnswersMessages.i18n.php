2340649b5ecc26c66b2794d12db92867f6e6fbf8
Andrew Steinborn <g33kdyoo@gmail.com>
2010-12-21 01:07:06 -0500

    Minor tweak

--- extensions/AnswersMessages/AnswersMessages.i18n.php
+++ extensions/AnswersMessages/AnswersMessages.i18n.php
@@ -1,7 +1,7 @@
 <?php
 $messages = array();
 $messages['en'] = array(
- 'sitenotice'=>'Input your question: <inputbox>type=create\npreload=MediaWiki:Newquestion\neditintro=MediaWiki:Newquestion2</inputbox>',
+ 'sitenotice'=>'Input your question: <inputbox>type=create preload=MediaWiki:Newquestion editintro=MediaWiki:Newquestion2</inputbox>',
  'newquestion'=>'[[Category:Un-answered]]',
  'newquestion2'=>'Please click "Save page".',
 );
